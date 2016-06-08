ff2b01216a61d3b58a9b79adf22b61b30683af92
Maidar Erdyneev <fragma18@gmail.com>
2016-06-08 04:49:12 +0300

    first mapper

--- src/main/java/com/company/main/MRIJ.java
+++ src/main/java/com/company/main/MRIJ.java
@@ -1,8 +1,12 @@
-package com.company;
+package com.company.main;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hive.ql.io.ORCFileStorageFormatDescriptor;
+import org.apache.hadoop.hive.ql.io.RCFileInputFormat;
+import org.apache.hadoop.hive.ql.io.orc.OrcInputFormat;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -12,6 +16,8 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hive.hcatalog.mapreduce.HCatInputFormat;
+import org.apache.hive.hcatalog.rcfile.RCFileMapReduceInputFormat;
 
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -19,7 +25,7 @@ import java.util.StringTokenizer;
 /**
  * Created by maydar on 27.12.15.
  */
-public class WordCount extends Configured implements Tool {
+public class MRIJ extends Configured implements Tool {
 
 
     public static class WordCountMap extends Mapper<Object, Text, Text, IntWritable> {
@@ -31,7 +37,6 @@ public class WordCount extends Configured implements Tool {
                 String nextToken = tokenizer.nextToken();
                 context.write(new Text(nextToken), new IntWritable(1));
             }
-
         }
     }
 
@@ -52,10 +57,21 @@ public class WordCount extends Configured implements Tool {
     }
 
     public int run(String[] args) throws Exception {
-        Job job = Job.getInstance(new Configuration(),
+
+        Configuration config = HBaseConfiguration.create();
+        Job job = Job.getInstance(config,
+                "MRIJ");
+
+        job.setInputFormatClass(RCFileMapReduceInputFormat.class);
+
+
+        /*
+
+                Configuration conf = new Configuration();
+
+        Job job = Job.getInstance(,
 
                 "wordcount");
-
 
         job.setOutputKeyClass(Text.class);
 
@@ -64,19 +80,23 @@ public class WordCount extends Configured implements Tool {
         job.setMapperClass(WordCountMap.class);
 
         job.setReducerClass(WordCountReduce.class);
-        
+
 
         FileInputFormat.setInputPaths(job, new Path(args[0]));
 
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-        job.setJarByClass(WordCount.class);
+        job.setJarByClass(MRIJ.class);
 
+        return job.waitForCompletion(true) ? 0 : 1;
+*/
+
+        job.setJarByClass(MRIJ.class);
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
     public static void main(String[] args) throws Exception {
-        System.exit(ToolRunner.run(new WordCount(), args));
+        System.exit(ToolRunner.run(new MRIJ(), args));
 
     }
 }
